d8bdeb1a3cd9f4450ac9e1eb19b6b4b88fbcffd2
Chas McMahon <ChasMcMahon@appacademy.io>
2014-04-24 13:00:01 -0400

    CSS improvements. Navbar Bracket Links done. Other forms better.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,49 @@ module ApplicationHelper
     HTML
   end
 
+  def bracket_link(text, url, action, appended_classes = [])
+    #should switch it so that it doesn't submit a form when going a 'GET'
+    classes = appended_classes.join(" ").html_safe #is html_safe necessary here?
+
+    action = action.upcase
+    if (action == 'GET')
+
+       <<-HTML.html_safe
+      <div class="bracket-link-wrap #{classes}">
+        <a class="bracket-link" href="#{url}">#{text}</a>
+      </div>
+       HTML
+
+    else
+
+      <<-HTML.html_safe
+  			<form class="bracket-link-wrap #{classes}"
+        action="#{url}" method="post">
+          #{set_form_method(action)}
+          #{auth_token}
+  			  <input class="bracket-link" type="submit" value="#{text}">
+  			</form>
+      HTML
+    end
+  end
+
+  def form_input(label_text, type, id, name, value)
+    <<-HTML.html_safe
+      <div class="input">
+        <label for="#{id}">#{label_text}</label>
+        <input type="#{type}" id="#{id}"
+          name="#{name}" value="#{value}">
+        </input>
+      </div>
+    HTML
+  end
+
+  def form_submit(text)
+    <<-HTML.html_safe
+     	<div class="submit">
+     		  <input type="submit" value="#{text}">
+     	</div>
+    HTML
+  end
+
 end
