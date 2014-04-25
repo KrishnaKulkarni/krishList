79ce3d6ae28e82d7dccf787a0919da7f49f87d78
Chas McMahon <ChasMcMahon@appacademy.io>
2014-04-25 14:53:41 -0400

    User posted_ads table styled close to goal/immensely better than before

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,26 +1,22 @@
 module UsersHelper
   def user_posts_list_item(appended_classes, options)
-    # if appended_classes.include?("table_head")
-    if true
-      options[:manage] = "manage"
-    else
-      options[:manage] = <<-LINKS
+    options[:manage] = <<-LINKS
           <a href="#{ad_url(options[:ad_id])}/repost">repost</a>
-          <form class="delete-ad"
-          action="#{ad_url(options[:ad_id])}" method="post">
-            #{auth_token}
-            <input type="hidden" name="_method" value="DELETE">
-            <input class="delete-ad" type="submit" value="delete">
-          </form>
-          LINKS
-    end
+    LINKS
+
+    #<form class="delete-ad"
+    # action="#{ad_url(options[:ad_id])}" method="post">
+    #   #{auth_token}
+    #   <input type="hidden" name="_method" value="DELETE">
+    #   <input class="delete-ad" type="submit" value="delete">
+    # </form>
 
     <<-HTML.html_safe
       <tr class="user-posts-list #{appended_classes.join(" ")}">
         <td class="status">#{options[:status]}</td>
         <td class="manage">#{options[:manage]}</td>
         <td class="title">#{options[:title]}</td>
-        <td class="region">#{options[:region]}</td>
+        <td class="region"><strong>#{options[:region]}</strong></td>
         <td class="category">#{options[:category]}</td>
         <td class="posted-date">#{options[:posted_date]}</td>
         <td class="responses">
@@ -30,6 +26,43 @@ module UsersHelper
         </td>
       </tr>
     HTML
+  end
+
+  # <% @user.posted_ads.each do |ad|  %>
+  #       <% options = {ad_id: ad.id,
+  #       status: 'Active',
+  #       title: "<a href="">#{h(ad.title)}</a>".html_safe,
+  #       region: ad.region,
+  #       category: ad.subcat.title,
+  #       posted_date: ad.created_at,
+  #       responses: ad.responses_count} %>
+  #     <%= user_posts_list_item([], options) %>
+  #   <% end  %>
+
+  def user_posts_list(ads)
+    output = ""
+    ads.each do |ad|
+      output += <<-HTML.html_safe
+      <tr class="user-posts-list">
+        <td class="status">Active</td>
+        <td class="manage"><a href="#{ad_url(ad)}/repost">repost</a></td>
+        <td class="title">
+          <a href="#{subcat_ad_url(ad.subcat, ad)}">
+            #{h(ad.title)}
+          </a>
+        </td>
+        <td class="region"><strong>#{h(ad.region)}</strong></td>
+        <td class="category">#{h(ad.subcat.title)}</td>
+        <td class="posted-date">#{h(ad.created_at)}</td>
+        <td class="responses">
+          <a href="#{user_ad_responses_url(current_user, ad)}">
+            #{ad.responses_count}
+          </a>
+        </td>
+      </tr>
+    HTML
+    end
+    output.html_safe
   end
 
   def account_header(user)
